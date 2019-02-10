2181116e0572841d4e7c38f380f50e12595b3e86
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-02-09 23:47:19 -0700

    Comments

--- restart-crap-apps.sh
+++ restart-crap-apps.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
+# State which applications we want to restart
 declare -a crap_apps=("Microsoft Outlook" "Microsoft Teams")
 
 for app_name in "${crap_apps[@]}"; do
+  # Gracefully stop the application
   printf "Stopping %s" "${app_name}"
   osascript -e "quit app \"${app_name}\""
+  # We'll wait 5 seconds (checking 10 times every half second)
+  # if five seconds elapses we force the app to close
   count=0
   while pgrep "${app_name}" >/dev/null; do
     printf "."
@@ -14,6 +18,7 @@ for app_name in "${crap_apps[@]}"; do
       pkill -3 "${app_name}"
     fi
   done
+  # Now that the application is closed, we start it back up
   printf "\nStarting %s\n" "${app_name}"
   open -a "${app_name}"
 done
